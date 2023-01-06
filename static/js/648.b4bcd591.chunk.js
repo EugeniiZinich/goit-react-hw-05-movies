2559145f84f071159900de99b55d398a57b24321
EugeniiZinich <104194714+EugeniiZinich@users.noreply.github.com>
2023-01-06 08:31:11 +0000

    Deploying to gh-pages from @ EugeniiZinich/goit-react-hw-05-movies@1136e0671958cc118af523cb3851668f2d9f5307 🚀

---
+++ static/js/648.b4bcd591.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[648],{648:function(e,t,n){n.r(t);var r=n(87),c=n(184);t.default=function(){return(0,c.jsxs)("main",{style:{textAlign:"center"},children:[(0,c.jsx)("b",{style:{fontSize:60},children:"404"}),(0,c.jsx)("p",{children:"Page is no found"}),(0,c.jsx)(r.OL,{to:"/",children:"Back to HOME page"})]})}}}]);
+//# sourceMappingURL=648.b4bcd591.chunk.js.map
